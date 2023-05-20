538b3811e67402703daffe51f7e9bada6f5e11cc
nicmackenzie <nicmackenzie@live.com>
2023-05-20 09:38:11 +0300

    completed assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React from 'react';
 
 function Item({ name, category }) {
+  const [added, setAdded] = React.useState(false);
   return (
-    <li className="">
+    <li className={added ? 'in-cart' : ''}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={() => setAdded(prev => !prev)}>
+        {added ? 'Remove From Cart' : 'Add to Cart'}
+      </button>
     </li>
   );
 }
